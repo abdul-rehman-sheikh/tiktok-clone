1b8d17f1c980f7c4f3606d2ded8fd7af95406698
MACBOOK <macbook@Hyatt.local>
2023-01-06 23:05:14 +0500

    tiktok server commit

---
+++ data.js
@@ -0,0 +1,24 @@
+const data = [
+    {
+        url:
+            "https://v16-webapp.tiktok.com/3a4ac64aa4944bce083501e7a52684d2/63b323a8/video/tos/maliva/tos-maliva-ve-0068c799-us/c8e27888adc44d76ac52f81a9946bb78/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0&cv=1&br=2186&bt=1093&cs=0&ds=3&ft=hatGN2zOjVQ9wnh9wDhd.7AUkX4gTsU9pXzFb4F_EeC&mime_type=video_mp4&qs=0&rc=OzU6N2c6NDk8Z2Y5ZDZnaEBpanY6eDw6ZmZzaDMzZzczNEBgYDZiYS5jNjQxNTQuLy5eYSMyYHMzcjQwYWVgLS1kMS9zcw%3D%3D&l=2023010212331495E4E8EF0C434EFA757F&btag=80000",
+        channel: "sssangha",
+        song: "awesome song from playlist",
+        likes: "123",
+        messages: "234",
+        description: "this works!!",
+        shares: "668"
+    },
+    {
+        url:
+            "https://v16-webapp.tiktok.com/3a4ac64aa4944bce083501e7a52684d2/63b323a8/video/tos/maliva/tos-maliva-ve-0068c799-us/c8e27888adc44d76ac52f81a9946bb78/?a=1988&ch=0&cr=0&dr=0&lr=tiktok_m&cd=0%7C0%7C1%7C0&cv=1&br=2186&bt=1093&cs=0&ds=3&ft=hatGN2zOjVQ9wnh9wDhd.7AUkX4gTsU9pXzFb4F_EeC&mime_type=video_mp4&qs=0&rc=OzU6N2c6NDk8Z2Y5ZDZnaEBpanY6eDw6ZmZzaDMzZzczNEBgYDZiYS5jNjQxNTQuLy5eYSMyYHMzcjQwYWVgLS1kMS9zcw%3D%3D&l=2023010212331495E4E8EF0C434EFA757F&btag=80000",
+        channel: "survivor",
+        song: "not so awesome song from playlist",
+        likes: "12",
+        messages: "2224",
+        description: "this will never works!!",
+        shares: "23245"
+    },
+];
+
+export default data;
